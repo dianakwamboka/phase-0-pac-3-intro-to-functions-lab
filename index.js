ff0d6e14653ef997301cb2ebb25de8ca0b19a982
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-13 15:08:32 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout() {
+    return string.toUpperCase('');
+}
+function whisper() {
+    return string.toLowerCase();
+}
+function logShout () {
+    console.log ().toUpperCase
+
+}
+
+
